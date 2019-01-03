53780c3b8ff7771f45665d8fbd35c80f405f5684
Cowie22 <rjcowie@ucla.edu>
2019-01-02 16:13:58 -0800

    first stab

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var element = document.body;
+  var output = [];
+  
+  var helperFunction = function(node) {
+    if(node.classList.contains(className)){
+      output.push(node);
+    }
+    for(var i = 0; i < node.childNodes.length; i++){
+      //if the nodeName is '#text" if might have another '#text' within it. So, we are calling it recursively. 
+      if(node.childNodes[i].nodeName !=='#text'){
+        helperFunction(node.childNodes[i]);
+      }
+    }
+  }
+  helperFunction(element);
+  return output;
 };
